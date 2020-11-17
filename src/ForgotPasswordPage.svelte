3f64d010ddcb16cdd304be90c4cbd3564f065380
Matthew Hirsch <hirschmatthew9@gmail.com>
2020-11-16 20:38:20 -0500

    Update ForgotPasswordPage.svelte

--- src/ForgotPasswordPage.svelte
+++ src/ForgotPasswordPage.svelte
@@ -3,6 +3,7 @@
         text-transform: uppercase;
     }
 </style>
+
 <div id="page-title">Forgot Your Password?</div>
 <form on:submit|preventDefault={() => alert("Password Submit Button was Clicked!!")}>
     <input id="email-input" type="email" />
