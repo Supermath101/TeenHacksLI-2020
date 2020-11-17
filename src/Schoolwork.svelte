939e549f7d5f3fec2c8971b72154e6e8e9bd9e89
Yoclad <60623032+Yoclad@users.noreply.github.com>
2020-11-16 23:50:08 -0500

    Update Schoolwork.svelte

---
+++ src/Schoolwork.svelte
@@ -0,0 +1,54 @@
+<script>
+</script>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Kodchasan&display=swap');
+	main {
+		background: #444;
+		width: 100%;
+		height: 100vh;
+		color: #FF6347;
+		font-family: "Kodchasan", sans-serif;
+	}
+
+	header {
+		width: 500px;
+	}
+	
+	.center {
+		text-align: center; 
+	}
+	
+	.right {
+		text-align: right;
+	}
+	
+	#items {
+		background: white;
+		width: 40%;
+		margin: 100px;
+		padding: 100px; 
+	}
+	
+	#Pagetitle {
+		
+		padding: 100px;
+		font-size: 1.5rem;
+	}
+	
+	#Bujamain {
+		text-decoration: none;
+	  color: #FF6347;
+		padding: 10px;
+		font-size: 2.5rem;
+		background: white;
+	}
+</style>
+
+<main>
+	<header>
+		<a class = "topleft" id="Bujamain"href="#linktohomepage"><span>Buja</span></a>
+	</header>
+	<span id="Pagetitle">Schoolwork</span>
+	<div id="items">insert list with each assignment input here</div>
+</main>
